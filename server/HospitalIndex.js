a14c3a5a520f49d2c92980271d6c0fc465dfc3a7
Abigail Watson <awatson1978@gmail.com>
2020-04-15 18:35:24 -0500

    2dsphere index; settings file to initialize

--- server/HospitalIndex.js
+++ server/HospitalIndex.js
@@ -46,7 +46,15 @@ Meteor.startup(function(){
   if(get(Meteor, 'settings.private.initializeHospitalIndex') === true)
   console.log('Server is set to auto-initialize hospital index.')
 
-  if(HospitalLocations.find().count() === 0){
+  // if(Locations){
+  //   Locations._ensureIndex({ "_location": "2dsphere"});
+  // }
+  // if(HospitalLocations){
+  //   HospitalLocations._ensureIndex({ "_location": "2dsphere"});
+  // }
+
+  // if(HospitalLocations.find().count() === 0){
+  if(get(Meteor, 'settings.private.initializeHospitalIndex'))
     console.log('Hospital index is empty.  Beginning initialization sequence.')
     Meteor.call('initializeHospitalIndex');
   }
